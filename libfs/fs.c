ba2600b3c4489037a8eceb9e931b58f5bfe2f968
yuan_3party <yapan@ucdavis.edu>
2018-05-24 03:17:37 -0700

    try fs_mount

--- libfs/fs.c
+++ libfs/fs.c
@@ -26,7 +26,8 @@ const static char FS_NAME[8] = "ECS150FS";
 #define FS_NAME "ECS150FS"
 
 
-#define eprintf(format, …) fprintf (stderr, format, ##__VA_ARGS__)
+#define eprintf(format, ...) \
+    fprintf (stderr, format, ##__VA_ARGS__)
 /* Superbock
 * Holds statistics about the filesystem 
 * according to the requirment 8 + 2 + 2 + 2 + 2 + 1 + 4079 = 4096; one block
@@ -119,7 +120,7 @@ int fs_mount(const char *diskname)
     struct SuperBlock * sp = malloc(BLOCK_SIZE);
     if(block_read(BLOCK_SIZE, (void *)sp) < 0) 
         return -1;
-    
+
     eprintf("signature: %s",sp->signature);
     eprintf("total_blk_count: %d",sp->total_blk_count);
     eprintf("rdir_blk: %d",sp->rdir_blk);
