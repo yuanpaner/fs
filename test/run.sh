690fb8eb764ff64304f2bafe8ce47ae73e49b40b
Yuan Pan <circle@pc28.cs.ucdavis.edu>
2018-05-24 17:12:46 -0700

    minor

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,5 @@
 make clean
 make
-../fs_ref.x info ../disk2 # > output_ref
-./test_fs.x info ../disk2 # > output_my
+./fs_ref.x info disk  # > output_ref
+./test_fs.x info disk # > output_my
 # diff output_ref output_my
---
+++ test/test_fs.d
@@ -0,0 +1,1 @@
+test_fs.o: test_fs.c ../libfs/fs.h
