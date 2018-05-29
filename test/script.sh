0831a77c6a538ed0da2fb3ac160d1aa5f0c3704b
Yuan Pan <circle@pc38.cs.ucdavis.edu>
2018-05-28 18:54:49 -0700

    script study, script.sh

---
+++ test/script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+# http://www.runoob.com/linux/linux-shell-passing-arguments.html
+
+echo "parameter pass example"
+echo "the first is the file name : $0"
+echo "1st argc: $1"
+echo "2nd argc: $2"
+echo "3rd argc: $3"
