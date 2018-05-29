10dbbd4f86cdae1009fe611bc85e1856a6f03e3e
yuan_3party <yapan@ucdavis.edu>
2018-05-29 02:08:24 -0700

    fail return again

--- libfs/fs.c
+++ libfs/fs.c
@@ -365,7 +365,7 @@ int init_alloc(){
 */
 
 
-/* version 1. 0  
+/* version 1. 0   */
 int fs_mount(const char *diskname)
 {
 	if (block_disk_open(diskname) != 0) return -1;
@@ -428,9 +428,9 @@ int fs_mount(const char *diskname)
 
 	return 0;
 }
-*/
+/**/
 
-/* version 2.0 fail , it fails at info */
+/* version 2.0 fail , it fails at info 
 int fs_mount(const char *diskname)
 {
     if (block_disk_open(diskname) != 0) return -1;
@@ -473,7 +473,10 @@ int fs_mount(const char *diskname)
 
     return 0;
 }
-/**/
+*/
+
+
+
 /**
  * fs_umount - Unmount file system
  *
