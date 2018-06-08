9e3f963de9ed0962a960e993bf4a5150949abd9c
yuan_3party <yapan@ucdavis.edu>
2018-06-08 00:59:24 -0700

    organize

--- test/script.sh
+++ test/script.sh
@@ -8,3 +8,6 @@ echo "the first is the file name : $0"
 echo "1st argc: $1"
 echo "2nd argc: $2"
 echo "3rd argc: $3"
+
+# https://linuxconfig.org/create-a-random-character-text-file-using-linux-shell
+< /dev/urandom tr -dc "X" | head -c4096 > hello
